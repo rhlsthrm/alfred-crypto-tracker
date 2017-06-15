46e7d593d57ccd3fd5380239ee732ac385a68f0a
Rahul Sethuram <rksethuram9@gmail.com>
2017-06-15 09:45:41 -0700

    Add logos, clean up title, add subtitle

--- crypto-tracker.py
+++ crypto-tracker.py
@@ -1,8 +1,17 @@
 # encoding: utf-8
 
 import sys
-from workflow import Workflow, ICON_WEB, web
+from workflow import Workflow, ICON_WEB, ICON_ERROR, web
 
+def format_strings_from_quote(ticker, quote_data):
+    usd_query = 'USDT_' + ticker.upper()
+    last = '%.2f' % float(quote_data[usd_query]['last'])
+    high = '%.2f' % float(quote_data[usd_query]['high24hr'])
+    low = '%.2f' % float(quote_data[usd_query]['low24hr'])
+    formatted = {}
+    formatted['title'] = ticker.upper() + ': $' + last
+    formatted['subtitle'] = '24hr High: ' + high + ' 24hr low ' + low
+    return formatted
 
 def main(wf):
     # Get query from Alfred
@@ -11,7 +20,6 @@ def main(wf):
     else:
         query = None
 
-    # url = 'http://jsonplaceholder.typicode.com/posts'
     url = 'https://poloniex.com/public?command=returnTicker'
     r = web.get(url)
 
@@ -25,33 +33,37 @@ def main(wf):
         usd_query = 'USDT_' + query.upper()
         usd_link = 'https://poloniex.com/exchange#' + usd_query
         if usd_query in result:
-            wf.add_item(title=query.upper() + ': $' + result[usd_query]['last'],
-                    subtitle=result[usd_query]['high24hr'],
-                    arg=usd_link,
-                    valid=True,
-                    icon=ICON_WEB)
+            formatted = format_strings_from_quote(query, result)
+            wf.add_item(title=formatted['title'],
+                        subtitle=formatted['subtitle'],
+                        arg=usd_link,
+                        valid=True,
+                        icon=ICON_WEB)
         else:
             wf.add_item(title='Couldn\'t find a quote for that symbol.',
-                    subtitle='Please try again.',
-                    icon=ICON_WEB)
+                        subtitle='Please try again.',
+                        icon=ICON_ERROR)
     else:
-        wf.add_item(title='BTC: $' + result['USDT_BTC']['last'],
-                    subtitle=result['USDT_BTC']['high24hr'],
+        formatted = format_strings_from_quote('BTC', result)
+        wf.add_item(title=formatted['title'],
+                    subtitle=formatted['subtitle'],
                     arg='https://poloniex.com/exchange#usdt_btc',
                     valid=True,
-                    icon=ICON_WEB)
+                    icon='icon/btc.png')
 
-        wf.add_item(title='ETH: $' + result['USDT_ETH']['last'],
-                    subtitle=result['USDT_ETH']['high24hr'],
+        formatted = format_strings_from_quote('ETH', result)
+        wf.add_item(title=formatted['title'],
+                    subtitle=formatted['subtitle'],
                     arg='https://poloniex.com/exchange#usdt_eth',
                     valid=True,
-                    icon=ICON_WEB)
+                    icon='icon/eth.png')
 
-        wf.add_item(title='LTC: $' + result['USDT_LTC']['last'],
-                    subtitle=result['USDT_LTC']['high24hr'],
+        formatted = format_strings_from_quote('LTC', result)
+        wf.add_item(title=formatted['title'],
+                    subtitle=formatted['subtitle'],
                     arg='https://poloniex.com/exchange#usdt_ltc',
                     valid=True,
-                    icon=ICON_WEB)
+                    icon='icon/ltc.png')
 
     # Send the results to Alfred as XML
     wf.send_feedback()
