0a062075e080fb9af97e00340dec361c01d3f395
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-02-01 11:34:03 -0800

    Switch to cryptocompare API

--- src/crypto-tracker.py
+++ src/crypto-tracker.py
@@ -3,17 +3,18 @@
 import sys
 from workflow import Workflow, ICON_WEB, ICON_ERROR, web
 
+
 def format_strings_from_quote(ticker, quote_data):
-    usd_query = 'USDT_' + ticker.upper()
-    last = "{:,.2f}".format(float(quote_data[usd_query]['last']))
-    high = "{:,.2f}".format(float(quote_data[usd_query]['high24hr']))
-    low = "{:,.2f}".format(float(quote_data[usd_query]['low24hr']))
-    change = '%.2f' % float(quote_data[usd_query]['percentChange'])
-    change = '+' + change if float(change) > 0 else change
+    data = quote_data["RAW"][ticker.upper()]["USD"]
+    price = "{:,.2f}".format(data["PRICE"])
+    high = "{:,.2f}".format(data["HIGH24HOUR"])
+    low = "{:,.2f}".format(data["LOW24HOUR"])
+    change = "{:,.2f}".format(data["CHANGEPCT24HOUR"])
     formatted = {}
-    formatted['title'] = ticker.upper() + ': $' + last + ' (' + change + '%)'
+    formatted['title'] = ticker.upper() + ': $' + price + ' (' + change + '%)'
     formatted['subtitle'] = '24hr High: $' + high + ' 24hr low $' + low
     return formatted
+
 
 def main(wf):
     # Get query from Alfred
@@ -22,49 +23,59 @@ def main(wf):
     else:
         query = None
 
-    url = 'https://poloniex.com/public?command=returnTicker'
-    r = web.get(url)
-
-    # throw an error if request failed
-    # Workflow will catch this and show it to the user
-    r.raise_for_status()
-
-    result = r.json()
     if query:
-        usd_query = 'USDT_' + query.upper()
-        usd_link = 'https://poloniex.com/exchange#' + usd_query
-        if usd_query in result:
-            formatted = format_strings_from_quote(query, result)
-            wf.add_item(title=formatted['title'],
-                        subtitle=formatted['subtitle'],
-                        arg=usd_link,
-                        valid=True,
-                        icon=ICON_WEB)
-        else:
+        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + \
+            query.upper() + '&tsyms=USD'
+        r = web.get(url)
+        # throw an error if request failed
+        # Workflow will catch this and show it to the user
+        r.raise_for_status()
+        result = r.json()
+        try:
+        formatted = format_strings_from_quote(query, result)
+        wf.add_item(title=formatted['title'],
+                    subtitle=formatted['subtitle'],
+                    arg='https://www.cryptocompare.com/coins/' + query + '/overview/USD',
+                    valid=True,
+                    icon=ICON_WEB)
+        except:
             wf.add_item(title='Couldn\'t find a quote for that symbol.',
                         subtitle='Please try again.',
                         icon=ICON_ERROR)
     else:
+        url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,BCH&tsyms=USD'
+        r = web.get(url)
+        # throw an error if request failed
+        # Workflow will catch this and show it to the user
+        r.raise_for_status()
+        result = r.json()
         formatted = format_strings_from_quote('BTC', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://poloniex.com/exchange#usdt_btc',
+                    arg='https://www.cryptocompare.com/',
                     valid=True,
                     icon='icon/btc.png')
 
         formatted = format_strings_from_quote('ETH', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://poloniex.com/exchange#usdt_eth',
+                    arg='https://www.cryptocompare.com/',
                     valid=True,
                     icon='icon/eth.png')
 
         formatted = format_strings_from_quote('LTC', result)
         wf.add_item(title=formatted['title'],
                     subtitle=formatted['subtitle'],
-                    arg='https://poloniex.com/exchange#usdt_ltc',
+                    arg='https://www.cryptocompare.com/',
                     valid=True,
                     icon='icon/ltc.png')
+
+        formatted = format_strings_from_quote('BCH', result)
+        wf.add_item(title=formatted['title'],
+                    subtitle=formatted['subtitle'],
+                    arg='https://www.cryptocompare.com/',
+                    valid=True,
+                    icon='icon/bch.png')
 
     # Send the results to Alfred as XML
     wf.send_feedback()
